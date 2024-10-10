670e0640487b7e048efa607194843a659f44dd29
martinekmichal <martinek.m.84@gmail.com>
2024-10-10 19:50:52 +0200

    update

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -9,12 +9,20 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
+from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
+MEDIA_ROOT =os.path.join(BASE_DIR, "media")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -117,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
